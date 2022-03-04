7c433a9835714cedc72465a6fc7ca8ee49f3312e
WAzaizeh <wesam.azaizeh@gmail.com>
2022-03-03 22:59:29 -0600

    Foundations Blocks & Procs Part I

---
+++ Software_Engineering_Foundations/wesam_azaizeh_blocks_project/lib/part_1.rb
@@ -0,0 +1,24 @@
+def select_even_nums(arr)
+    arr.select(&:even?)
+end
+
+def reject_puppies(hash_arr)
+    hash_arr.reject {|d| d["age"] <= 2}
+end
+
+def count_positive_subarrays(arr)
+    arr.count {|sub_arr| sub_arr.sum > 0}
+end
+
+def aba_translate(str)
+    vowels = 'aeiou'
+    res = ''
+    str.chars.map.with_index {|c, i| vowels.include?(c) ? res += (c + 'b' + c) : res += c}.join
+    res
+end
+
+def aba_array(arr)
+    arr.map {|w| aba_translate(w)}
+end
+
+p aba_array(["cat", "dog", "butterfly"])
