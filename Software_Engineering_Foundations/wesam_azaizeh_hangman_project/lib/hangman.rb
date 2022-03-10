a5a56a84db97bf9150e9b5b749f843af8526c8e5
WAzaizeh <wesam.azaizeh@gmail.com>
2022-03-09 23:52:20 -0500

    Foundations Input/ Output Part III

--- Software_Engineering_Foundations/wesam_azaizeh_hangman_project/lib/hangman.rb
+++ Software_Engineering_Foundations/wesam_azaizeh_hangman_project/lib/hangman.rb
@@ -14,8 +14,6 @@ class Hangman
     @remaining_incorrect_guesses = 5
   end
 
-  
-
   def guess_word
     @guess_word
   end
@@ -41,8 +39,54 @@ class Hangman
     arr.each {|i| @guess_word[i] = c}
   end
 
+  ## PART 2 ## 
 
+  def try_guess(c)
+    if already_attempted?(c)
+      puts 'that has already been attempted'
+      false
+    else
+      @attempted_chars << c
+      if @secret_word.include?(c)
+         idx_arr = get_matching_indices(c)
+        fill_indices(c, idx_arr)
+      else
+        @remaining_incorrect_guesses -= 1
+      end
+      true
+    end
+  end
+
+  def ask_user_for_guess
+    puts 'Enter a char:'
+    c = gets.chomp
+    try_guess(c)
+  end
+
+  def win?
+    if @guess_word.join == @secret_word
+      puts 'WIN'
+      true
+    else
+      false
+    end
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      puts 'LOSE'
+      true
+    else
+      false
+    end
+  end
+
+  def game_over?
+    if win? || lose?
+      puts @secret_word
+      true
+    else false
+    end
+  end
 
 end
-
-p Hangman.random_word
