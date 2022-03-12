a1ed6fa6bb5ec5408e3fc752ebf41b65e9a9dc68
WAzaizeh <wesam.azaizeh@gmail.com>
2022-03-12 16:02:11 -0500

    Foundations OOP Part V

--- Software_Engineering_Foundations/wesam_azaizeh_mastermind_project/lib/code.rb
+++ Software_Engineering_Foundations/wesam_azaizeh_mastermind_project/lib/code.rb
@@ -6,6 +6,8 @@ class Code
     "Y" => :yellow
   }
 
+  ## PART 1 ##
+
   attr_reader :pegs
 
   def self.valid_pegs?(chars)
@@ -21,8 +23,8 @@ class Code
   end
 
   def self.from_string(str)
-    pegs = str.each_char
-    Code.valid_pegs?(pegs) ? Code.new( pegs) : (raise 'contains invalide peg(s)!')
+    pegs = str.chars.to_a
+    Code.valid_pegs?(pegs) ? Code.new(pegs) : (raise 'contains invalide peg(s)!')
   end
 
   def [](idx)
@@ -33,4 +35,35 @@ class Code
     @pegs.size
   end
 
+  ## PART 2 ##
+
+  def num_exact_matches(code_2)
+    (0 ... self.pegs.length).count{|i| self.pegs[i] == code_2.pegs[i]}
+  end
+
+  def num_near_matches(code_2)
+    include_idx = self.pegs.each_index.select{|i| self.pegs[i] != code_2.pegs[i]}
+    if include_idx.length < 2
+       return 0
+    else
+      to_match = @pegs.values_at(*include_idx)
+      c = 0
+      include_idx.map do |i|
+         if to_match.include?(p code_2.pegs[i])
+          to_match.delete_at( to_match.index(code_2.pegs[i]))
+          c += 1
+         end
+        end
+        c
+    end
+  end
+
+  def ==(code_2)
+    return code_2.length == self.num_exact_matches(code_2)
+  end
+
 end
+
+# code_1 = Code.from_string('RGRB')
+# code_2 = Code.new(["G", "R", "R", "R"])
+# p code_1.num_near_matches(code_2)
