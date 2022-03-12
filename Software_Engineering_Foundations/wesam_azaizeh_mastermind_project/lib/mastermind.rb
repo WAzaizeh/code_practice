8dcca5122e3061332d47f899c4ab870b671090a4
WAzaizeh <wesam.azaizeh@gmail.com>
2022-03-12 16:10:50 -0500

    Foundations OOP Part VI

--- Software_Engineering_Foundations/wesam_azaizeh_mastermind_project/lib/mastermind.rb
+++ Software_Engineering_Foundations/wesam_azaizeh_mastermind_project/lib/mastermind.rb
@@ -2,4 +2,23 @@ require_relative "code"
 
 class Mastermind
 
+    ### PART 3 ##
+
+    def initialize(len)
+        @secret_code = Code.random(len)
+    end
+
+    def print_matches(code_2)
+        puts @secret_code.num_exact_matches(code_2)
+        puts @secret_code.num_near_matches(code_2)
+    end
+
+    def ask_user_for_guess
+        puts 'Enter a code'
+        guess = gets.chomp
+        guess_code = Code.from_string(guess)
+        print_matches(guess_code)
+        @secret_code.num_exact_matches(guess_code) == guess.size
+    end
+
 end
